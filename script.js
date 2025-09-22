419c8b3f096ec594cff09f36829a043894234378
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-22 23:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,12 +8,23 @@ let cell1=document.getElementById("title").value;
 let cell2=document.getElementById("author").value;
 let cell3=document.getElementById("isbn").value;
 const new_row=tbl.insertRow();
-const newcell1=new_row.insertCell()
+const new_button=document.createElement('button');
+	new_button.textContent='x';
+	new_button.classList.add('action-button');
+	new_button.addEventListener('click',function(){
+		
+		if(tbl.rows.length>1 )
+		{
+			
+			tbl.deleteRow(-1);
+		}
+	})
+	const newcell1=new_row.insertCell()
 const newcell2=new_row.insertCell()
 const newcell3=new_row.insertCell()
 const newcell4=new_row.insertCell()
 newcell1.innerHTML=cell1;
 newcell2.innerHTML=cell2;
 newcell3.innerHTML=cell3;
-newcell4= <input type="button" class="delete" value="x">
+newcell4.appendChild(new_button);
 }
